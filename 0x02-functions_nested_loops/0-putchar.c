1877519a5433a89bd46be714882a7e4c2ff840fa
JoanTebugulwa <tebugulwajoan@gmail.com>
2023-04-25 15:48:58 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main: - A program that prints _putchar, followed by a new line
+ * written bt Tjoan
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u')
+_putchar('\n');
+_putchar('h');
+_putchar('a');
+_putchar('c');
+_putchar('r');
+_putchar('t');
+return (0);
+}
