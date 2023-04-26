c334511ca3bdebd1461768aa6dc0fde13f146cc4
JoanTebugulwa <tebugulwajoan@gmail.com>
2023-04-26 13:24:05 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print _alphabet -  A function that prints the alphabet
+ * written by TJ
+ * Return: 0 (SUCCESS)
+ */
+void  print_alphabet(void)
+
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+_putchar(alphabet);
+
+_putchar('\n')
+return (0);
+}
