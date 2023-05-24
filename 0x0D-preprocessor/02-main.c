1fac45e2c00bd9b31b8e6cb67e91f2a3ae08bfc7
JoanTebugulwa <tebugulwajoan@gmail.com>
2023-05-23 19:01:17 -0700

    Processor

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - a programme taht prints a name it was compiled form
+ * Return 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
